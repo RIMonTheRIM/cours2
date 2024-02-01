683630e8f149f740ab0b5715bfad32efd115ee8d
Ihasiniravo "RIME <silverlion2424@gmail.com>
2024-02-01 13:39:29 +0000

    mise à jour maj et minuscule

--- email.js
+++ email.js
@@ -1,22 +1,35 @@
 let emailTxt;
+
 document.getElementById("gen").onclick = function(){
-    const date = new Date();
-    nom = document.getElementById("Nom").value;
-    prenom = document.getElementById("Prenom").value;
-    dateTxt =date.getFullYear();
-    emailTxt = prenom.charAt(0)+nom+dateTxt+"@supinfo.com";
-    emailTxt = emailTxt.toUpperCase();
-    console.log(emailTxt);
-    
-    document.getElementById("email").setAttribute("value",emailTxt);
-    let labelList = document.querySelectorAll("label");
-    for(var i = 0; i<labelList.length;i++){
-        labelList[i].style.color = "rgb(56, 207, 56)";
+
+    let nom = document.getElementById("Nom");
+    let prenom = document.getElementById("Prenom");
+
+        const date = new Date();
+        dateTxt =date.getFullYear();
+        emailTxt = prenom.value.charAt(0)+nom.value+dateTxt+"@supinfo.com";
+        emailTxt = emailTxt.toUpperCase();
+        console.log(emailTxt);
+        document.getElementById("email").value = emailTxt;
+
+        let labelList = document.querySelectorAll("label");
+        for(var i = 0; i<labelList.length;i++){
+            labelList[i].style.color = "rgb(56, 207, 56)";
+            let valueTxt = labelList[i].innerHTML;
+            valueTxt = valueTxt.toUpperCase();
+            labelList[i].innerHTML = valueTxt;
+        }
     }
-}
+
 document.getElementById("eff").onclick = function(){
     document.getElementById("Prenom").value = "";
     document.getElementById("Nom").value = "";
-    //document.getElementById("email").setAttribute("value","");
     document.getElementById("email").value = "";
+    let labelList = document.querySelectorAll("label");
+        for(var i = 0; i<labelList.length;i++){
+            labelList[i].style.color = "rgb(0, 0,0)";
+            let valueTxt = labelList[i].innerHTML;
+            valueTxt = valueTxt.toLowerCase();
+            labelList[i].innerHTML = valueTxt;
+        }
 }
